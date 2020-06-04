09fd50d9e62572a2710d35a799c3ad2d9f722ba3
Naniloco323 <60305187+Naniloco323@users.noreply.github.com>
2020-06-04 22:11:55 +0300

    04

--- M01S04/ex05/app.js
+++ M01S04/ex05/app.js
@@ -1,12 +1,11 @@
 var i;
-var testNumber = window.prompt('introdu numarul:') || 2;
-var upperLimit = window.prompt('introdu limita') || 10;
+var testNumber = window.prompt('Introdu numarul: ') || 2;
+var upperLimit = window.prompt('Introdu limita: ') || 10;
 
-for (i = 1; i <= upperLimit; i++) {
-    
-    console.log(i);
-    if (i % testNumber !== 0) {
-        continue;
-    }
-    console.log(`Acest numar este multiplu de ${testNumber}.`);
+for (i = 0; i <= upperLimit; i++) {
+  console.log(i);
+  if (i % testNumber !== 0) {
+    continue;
+  }
+  console.log(`Acest numar este multiplu de ${testNumber}.`);
 }
