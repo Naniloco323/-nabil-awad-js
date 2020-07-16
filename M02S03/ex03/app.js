f3f929be4cdd4dad7bd2082e71e13ac93c146f9a
Naniloco323 <60305187+Naniloco323@users.noreply.github.com>
2020-07-16 15:18:15 +0300

    0

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -6,20 +6,9 @@
 let countIn = 0;
 let countOut = 0;
 
-
 let createMessage = (countIn, countOut) => {
   return `In: ${countIn}, Out: ${countOut}`;
 };
-
-let createSecondMessage = (countIn, countOut) => {
-  return `Mouseul a trecut de ${countIn + countOut}`;
-}
-
-
-
-
-
-
 
 let p = document.createElement('p');
 document.body.prepend(p);
@@ -31,26 +20,18 @@ let p2 = document.createElement('p');
 p2.innerText = createMessage(countIn, countOut);
 p.after(p2);
 
-let p3 = document.createElement('p');
-p3.innerText = createSecondMessage(countIn, countOut);
-p2.after(p3);
-
 stageElement.addEventListener('mouseover', () => {
   let message = 'Mouseul este pe scena';
   countIn++;
-  
 
   p2.innerText = createMessage(countIn, countOut);
   p.innerText = message;
-  p3.innerText = createSecondMessage(countIn, countOut);
 });
 
 stageElement.addEventListener('mouseout', () => {
   let message = 'Mouseul nu este pe scena';
   countOut++;
-  
 
   p2.innerText = createMessage(countIn, countOut);
   p.innerText = message;
-  p3.innerText = createSecondMessage(countIn, countOut);
 });
